b833067f394312591a899ac40821af8c20cdb01c
erik-vm <60032776+erik-vm@users.noreply.github.com>
2022-09-05 19:12:30 +0300

    added faker dependency and created method inside DataInit to prepopulate database with users, using faker

--- src/main/java/com/pilgrims/travelagency/models/Continent.java
+++ src/main/java/com/pilgrims/travelagency/models/Continent.java
@@ -1,22 +1,18 @@
 package com.pilgrims.travelagency.models;
 
-import com.pilgrims.travelagency.utils.Constants;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 
-import javax.lang.model.element.Name;
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Data
 @EqualsAndHashCode(callSuper = true)
-public class Continents extends Auditable<String> implements Serializable {
+public class Continent extends Auditable<String> implements Serializable {
     private static final long serialVersionID = 1L;
 
     @Id
